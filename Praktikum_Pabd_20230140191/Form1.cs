2d2150f13c1f8771b1526804d049b3c31532bef2
dutabrahmana <dutalinggau25@gmail.com>
2025-03-21 13:21:41 +0700

    Ini Untguk deklarasi deklarasi connection string di C# untuk menghubungkan aplikasi Windows Forms kamu dengan database SQL Serve

--- Praktikum_Pabd_20230140191/Form1.cs
+++ Praktikum_Pabd_20230140191/Form1.cs
@@ -12,6 +12,9 @@ namespace Praktikum_Pabd_20230140191
 {
     public partial class Form1 : Form
     {
+        private string connectionString = "Data Source=LAPTOP-KJQI23G9\\DUTA;Initial Catalog=OrganisasiMahasiswa;Integrated Security=True";
+        public Form1()
+
         public Form1()
         {
             InitializeComponent();
