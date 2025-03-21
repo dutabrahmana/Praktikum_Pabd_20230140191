fb01829759e7fda8f8a30fe27dc232f203692913
dutabrahmana <dutalinggau25@gmail.com>
2025-03-21 15:52:17 +0700

    Mengambil data dari tabel Mahasiswa. AS [NIM] membuat alias untuk kolom NIM agar nama kolom tetap NIM di DataGridView. Mengambil lima kolom: NIM, Nama, Email, Telepon, dan Alamat.

--- Praktikum_Pabd_20230140191/Form1.cs
+++ Praktikum_Pabd_20230140191/Form1.cs
@@ -43,6 +43,10 @@ namespace Praktikum_Pabd_20230140191
                 try
                 {
                     conn.Open();
-                
+                    string query = "SELECT NIM AS [NIM], Nama, Email, Telepon, Alamat FROM Mahasiswa";
+                    SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
 
 
