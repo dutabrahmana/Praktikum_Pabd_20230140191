e945873e6019041793f18bcac7d1e1f09c7ccd44
dutabrahmana <dutalinggau25@gmail.com>
2025-03-21 15:52:39 +0700

    DataTable (dt) digunakan untuk menampung hasil query dari database dalam bentuk tabel. Di sinilah data sementara akan disimpan sebelum ditampilkan di DataGridView.

--- Praktikum_Pabd_20230140191/Form1.cs
+++ Praktikum_Pabd_20230140191/Form1.cs
@@ -45,7 +45,8 @@ namespace Praktikum_Pabd_20230140191
                     conn.Open();
                     string query = "SELECT NIM AS [NIM], Nama, Email, Telepon, Alamat FROM Mahasiswa";
                     SqlDataAdapter da = new SqlDataAdapter(query, conn);
-           
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
 
 
 
