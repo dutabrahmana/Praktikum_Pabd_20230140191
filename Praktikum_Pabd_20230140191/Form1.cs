56731f5325e062568393f3bfad2ed2a7812046a6
dutabrahmana <dutalinggau25@gmail.com>
2025-03-21 15:31:26 +0700

    Fungsi: Mengecek apakah pengguna menekan tombol Yes di kotak dialog. Jika No, proses penghapusan dibatalkan.

--- Praktikum_Pabd_20230140191/Form1.cs
+++ Praktikum_Pabd_20230140191/Form1.cs
@@ -167,6 +167,7 @@ namespace Praktikum_Pabd_20230140191
             {
                 DialogResult confirm = MessageBox.Show("Yakin ingin menghapus data ini?", "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
                 if (confirm == DialogResult.Yes)
+
             
 
 
